cd0cc08caadf5b3f70eb9bfc2da489d7143e2702
Ch3did <phchedidk@gmail.com>
2021-03-10 13:15:24 -0300

    [FEATURE] - Arquivo de configuraçao Criado

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+
+#Constantes
+TIPO_DATABASE = 'mysql'
+USER_DATABASE = 'root'
+SENHA_DATABASE = 'forensics'
+PORTA = '3306'
+NOME_DATABASE = 'pagarMe'
+ECHO = True
+
+
+DATABASE_CONFIG = TIPO_DATABASE+'://'+USER_DATABASE +':'+SENHA_DATABASE+'@localhost:'+PORTA+'/'+NOME_DATABASE
