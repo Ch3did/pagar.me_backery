b5ac0c4e9f9b49e618c21e6768a94442e40dc1c6
Ch3did <phchedidk@gmail.com>
2021-03-10 13:56:36 -0300

    [FEATURE] - Adicionando a rota de sistema para criar o DB

--- handlers/system_handlers.py
+++ handlers/system_handlers.py
@@ -1,7 +1,19 @@
-from flask import jsonify
+from database.models import model
+from flask import jsonify, flash, redirect
+
+
+def home():
+  return '<h1>API FLASK</h1>'
+
+
 
 def handler_ping():
-  return jsonify({
+      return jsonify({
     'result': 'pong'
   })
 
+
+
+def handler_create_db():
+  model()
+  return redirect ('/')
