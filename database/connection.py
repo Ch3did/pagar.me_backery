ebc7968b5651ff28c452082eafbeaf34e4077c2b
Ch3did <phchedidk@gmail.com>
2021-03-10 13:55:37 -0300

    [UPDATE] - Atualizando o DB com mysql

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,11 @@
-import mysql.connector
+from settings import DATABASE_CONFIG, ECHO
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def get_connection(config):
-  mydb = mysql.connector.connect(
-    host=config["host"],
-    user=config["user"],
-    password=config["password"],
-    database=config["database"]
-  )
-  return mydb
-    
+
+engine = create_engine(DATABASE_CONFIG, echo=ECHO)
+
+Session = sessionmaker(bind=engine)
+
+
+session = Session()
