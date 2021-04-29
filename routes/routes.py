862ac7dbd430b0a3461d2b945171b41dcbff1ae4
Ch3did <phchedidk@gmail.com>
2021-04-29 01:36:25 -0300

    [UPDATE] - Organizing routes without the hadlers

---
+++ routes/routes.py
@@ -0,0 +1,56 @@
+
+def load_routes(app):
+    print(' * Loading Routes')
+
+                                                         # System Routes
+    #Home da API
+    @app.route('/')
+    def init():
+        return "<h1>Index</h1>"
+
+    #Ping pra teste de conexão
+    @app.route('/ping', methods=['GET'])
+    def ping():
+        pass
+        #return handler_ping()
+
+    #Cria a database para utilização da API
+    @app.route('/create_db',methods=['GET'])
+    def create_db():
+        pass
+        #return handler_create_db()
+
+                                                            #API Routes
+    #Mostra todos os produtos cadastrados                                                        
+    @app.route('/show_all_prod', methods=['GET'])
+    def show_all():
+        pass
+        #return handler_show_all()
+    
+    #Mostra os produtos filtrados por palavra-chave
+    @app.route('/show_targuet',methods=['GET'])
+    def show_one():
+        pass
+        #return handler_show_one()
+
+    #Cadastro de padarias    
+    @app.route('/cad_backery',methods=['POST'])
+    def new_backery():
+        pass
+        #return handler_new_backery()
+
+    #Login de padaria para cadastro ou exclusão de produtos
+    @app.route('/login',methods=['POST'])
+    def login():
+        pass
+        #return handler_login()
+
+    #Cadastro de novos produtos mediante Login
+    @app.route('/cad_prod',methods=['POST'])
+    def new_prod():
+        pass
+        #return handler_new_backery()
+
+    
+
+
