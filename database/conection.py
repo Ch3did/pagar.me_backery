118d7a61f9417a19309086c44fc4518a5bb910c8
Ch3did <phchedidk@gmail.com>
2021-04-29 13:30:55 -0300

    [UPDATE] - Formatting the connection status output

--- database/conection.py
+++ database/conection.py
@@ -1,12 +1,13 @@
 import sqlalchemy as db
-from config  import type, host, user, password, database
+from config  import typedb, host, user, password, database
 
 
 def connection():
     try:
-        engine = db.create_engine(type+"://"+user+":"+password+"@"+host+"/"+database)
+        engine = db.create_engine(typedb+"://"+user+":"+password+"@"+host+"/"+database)
+        print(" * Database Conectada" )
         return engine
     except:
-        print (" * Verifique as configurações dadatabase")
-        return null 
+        print(' * Verifique as configurações dadatabase!   **')
+        return None 
 
